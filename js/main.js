b54bae830fb0fc43cb62ded4c58c65d6ffb16641
Apolinapolis <larra12@ya.ru>
2024-05-15 16:13:27 +0300

    final

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const elems = document.querySelectorAll(".sidenav");
+  const instances = M.Sidenav.init(elems);
 
-document.addEventListener('DOMContentLoaded', function() {
-    let elems = document.querySelectorAll('.sidenav');
-    let instances = M.Sidenav.init(elems);
-  })
+  const modal = document.querySelectorAll(".modal");
+  const modalInit = M.Modal.init(modal);
+
+  const gallery = document.querySelectorAll(".materialboxed");
+  const gallery_init = M.Materialbox.init(gallery);
+})
